ab09e28ce3dd1246486d63be4583fca5a7b1e368
Gojaehyeon <gojaehyeonstudio@gmail.com>
2025-05-17 12:45:54 +0900

    Initial Commit

---
+++ DisplayGo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DisplayGo
+//
+//  Created by 고재현 on 5/17/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ DisplayGoTests/DisplayGoTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisplayGoTests.swift
+//  DisplayGoTests
+//
+//  Created by 고재현 on 5/17/25.
+//
+
+import Testing
+@testable import DisplayGo
+
+struct DisplayGoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
